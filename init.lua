498ba79932ce86578e0ffe8232a163f81a34d70f
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2024-02-08 22:53:07 -0300

    feat: updated spacing config

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+local vim = vim
+local opt = vim.opt
 -- local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
@@ -6,3 +8,18 @@
 --   command = "tabdo wincmd =",
 -- })
 vim.wo.relativenumber = true
+
+-- opt.foldmethod = "expr"
+-- opt.foldexpr = "nvim_treesitter#foldexpr()"
+
+-- tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+
+opt.belloff = "all" -- Just turn the dang bell off
+
+opt.clipboard = "unnamedplus"
+
+opt.inccommand = "split"
+opt.swapfile = false -- Living on the edge
